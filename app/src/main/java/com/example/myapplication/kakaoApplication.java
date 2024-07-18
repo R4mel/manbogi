5cfc6a6b88f129933a2c88ba05f89c2af8568fd6
Ramel <dev_20@naver.com>
2024-07-18 17:41:43 +0900

    kakao

--- app/src/main/java/com/example/myapplication/kakaoApplication.java
+++ app/src/main/java/com/example/myapplication/kakaoApplication.java
@@ -24,38 +24,8 @@ public class kakaoApplication extends Application {
         Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
             @Override
             public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
-                updateKakaoLoginUi();
                 return null;
             }
         };
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(login.this)) {
-                    UserApiClient.getInstance().loginWithKakaoTalk(login.this, callback);
-                } else {
-                    UserApiClient.getInstance().loginWithKakaoAccount(login.this, callback);
-                }
-            }
-        });
-    }
-
-    private void updateKakaoLoginUi() {
-        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
-            @Override
-            public Unit invoke(User user, Throwable throwable) {
-                if (user != null) {
-                    Log.d(TAG, "invoke: id" + user.getId());
-                    Log.d(TAG, "invoke: email" + user.getKakaoAccount().getEmail());
-                    Log.d(TAG, "invoke: nickname" + user.getKakaoAccount().getProfile().getNickname());
-                    Log.d(TAG, "invoke: gender" + user.getKakaoAccount().getGender());
-                    Log.d(TAG, "invoke: age" + user.getKakaoAccount().getAgeRange());
-                } else {
-                    Log.d(TAG, "로그인이 안되어 있습니다.");
-                }
-                return null;
-            }
-        });
     }
 }
